4cdb19d246eba9e2ab383ee2fcab509d69d5ab3e
Serikoff <serikoff@li.ru>
2019-04-13 18:25:09 +0300

    add brain-gcd

--- src/index.js
+++ src/index.js
@@ -10,16 +10,29 @@ export const startMessage = (message) => {
 };
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const getUserAnswer = qestion => readlineSync.question(`Question: ${qestion} `);
+
+const getUserAnswer = (question) => {
+  switch (question.game) {
+    case 'ever':
+      return readlineSync.question(`Question: ${question.a} => `);
+    case 'calc':
+      return readlineSync.question(`Question: ${question.a} ${question.operator} ${question.b} =>`);
+    case 'gcd':
+      return readlineSync.question(`Question: ${question.a} ${question.b} => `);
+    default: return 'error';
+  }
+};
+
 const isEver = number => number % 2 === 0;
+
 const startGame = (count, correctAnswerFun, userName, questionFun) => {
   if (count === 3) {
     console.log(`Congratulations, ${userName}!`);
     return;
   }
-  const qestion = questionFun();
-  const userAnswer = getUserAnswer(qestion);
-  const correctAnswer = correctAnswerFun(qestion);
+  const question = questionFun();
+  const userAnswer = getUserAnswer(question);
+  const correctAnswer = correctAnswerFun(question);
   console.log(`Your answer: ${userAnswer}`);
 
   if (correctAnswer === userAnswer) {
@@ -34,8 +47,18 @@ export const brainEven = () => {
   const userName = startMessage('Answer "yes" if number even otherwise answer "no".');
 
   const count = 0;
-  const questionFun = (() => getRandom(1, 21));
-  const correctAnswerFun = (number => (isEver(number) ? 'yes' : 'no'));
+  const questionFun = (() => {
+    const result = {
+      a: getRandom(1, 21),
+      game: 'ever',
+    };
+    return result;
+  });
+
+  const correctAnswerFun = ((question) => {
+    const number = question.a;
+    return (isEver(number) ? 'yes' : 'no');
+  });
 
   startGame(count, correctAnswerFun, userName, questionFun);
 };
@@ -43,30 +66,66 @@ export const brainEven = () => {
 export const brainCalc = () => {
   const userName = startMessage('What is the result of the expression?');
   const count = 0;
-  const qestionFun = (() => {
-    const a = getRandom(0, 10);
-    const b = getRandom(0, 10);
-    const operator = getRandom(0, 3);
+  const questionFun = (() => {
+    const result = {
+      a: getRandom(0, 10),
+      b: getRandom(0, 10),
+      operator: getRandom(0, 3),
+      game: 'calc',
+    };
 
-    switch (operator) {
-      case 0: return `${a} + ${b}`;
-      case 1: return `${a} * ${b}`;
-      case 2: return `${a} - ${b}`;
+    switch (result.operator) {
+      case 0:
+        result.operator = '+';
+        break;
+      case 1:
+        result.operator = '*';
+        break;
+      case 2:
+        result.operator = '-';
+        break;
+      default: result.operator = 'error';
+    }
+    return result;
+  });
+
+  const correctAnswerFun = ((question) => {
+    switch (question.operator) {
+      case '+': return String(question.a + question.b);
+      case '*': return String(question.a * question.b);
+      case '-': return String(question.a - question.b);
       default: return 'error';
     }
   });
-  const correctAnswerFun = ((qestion) => {
-    const a = Number(qestion[0]);
-    const b = Number(qestion[4]);
-    const operator = qestion[2];
 
-    switch (operator) {
-      case '+': return String(a + b);
-      case '*': return String(a * b);
-      case '-': return String(a - b);
-      default: return 'error';
-    }
+  return startGame(count, correctAnswerFun, userName, questionFun);
+};
+
+export const brainGCD = () => {
+  const userName = startMessage('Find the greatest common divisor of given numbers.');
+
+  const count = 0;
+  const questionFun = (() => {
+    const result = {
+      a: getRandom(1, 21),
+      b: getRandom(1, 21),
+      game: 'gcd',
+    };
+    return result;
   });
 
-  return startGame(count, correctAnswerFun, userName, qestionFun);
+  const correctAnswerFun = ((question) => {
+    const A = question.a;
+    const B = question.b;
+
+    const getGCD = (a, b) => {
+      if (b) {
+        return getGCD(b, a % b);
+      }
+      return Math.abs(a);
+    };
+    return String(getGCD(A, B));
+  });
+
+  startGame(count, correctAnswerFun, userName, questionFun);
 };
