3a2136eacf516957437a53cc7cdbb09d2f0abc82
Serikoff <serikoff@li.ru>
2019-04-13 10:43:31 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import readlineSync from 'readline-sync';
 
-export const start = (message) => {
+export const startMessage = (message) => {
   console.log('Welcome to the Brain Games!');
   console.log(message);
   const userName = readlineSync.question('May I have your name?  ');
@@ -10,75 +10,63 @@ export const start = (message) => {
 };
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const getUserAnswer = qestion => readlineSync.question(`Question: ${qestion} `);
+const isEver = number => number % 2 === 0;
+const startGame = (count, correctAnswerFun, userName, questionFun) => {
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}!`);
+    return;
+  }
+  const qestion = questionFun();
+  const userAnswer = getUserAnswer(qestion);
+  const correctAnswer = correctAnswerFun(qestion);
+  console.log(`Your answer: ${userAnswer}`);
+
+  if (correctAnswer === userAnswer) {
+    console.log('Correct!');
+    startGame(count + 1, correctAnswerFun, userName, questionFun);
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+  }
+};
 
 export const brainEven = () => {
-  const userName = start('Answer "yes" if number even otherwise answer "no".');
-  const isEver = number => number % 2 === 0;
+  const userName = startMessage('Answer "yes" if number even otherwise answer "no".');
 
-  const startBrainEven = (correctAnswersCount) => {
-    if (correctAnswersCount === 3) {
-      console.log(`Congratulations, ${userName}!`);
-      return;
-    }
+  const count = 0;
+  const questionFun = (() => getRandom(1, 21));
+  const correctAnswerFun = (number => (isEver(number) ? 'yes' : 'no'));
 
-    const number = getRandom(1, 21);
-    const userAnswer = readlineSync.question(`Question: ${number} `);
-    console.log(`Your answer: ${userAnswer}`);
-
-    const correctAnswer = isEver(number) ? 'yes' : 'no';
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-      startBrainEven(correctAnswersCount + 1);
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    }
-  };
-
-  startBrainEven(0);
+  startGame(count, correctAnswerFun, userName, questionFun);
 };
 
 export const brainCalc = () => {
-  const userName = start('What is the result of the expression?');
-
-  const getExpression = (a, b, operator) => {
-    switch (operator) {
-      case 0: return `(${a} + ${b})`;
-      case 1: return `(${a} * ${b})`;
-      case 2: return `(${a} - ${b})`;
-      default: return 'error';
-    }
-  };
-  const getCorrectAnswer = (a, b, operator) => {
-    switch (operator) {
-      case 0: return String(a + b);
-      case 1: return String(a * b);
-      case 2: return String(a - b);
-      default: return 'error';
-    }
-  };
-
-  const startBrainCalc = (correctAnswersCount) => {
-    if (correctAnswersCount === 3) {
-      console.log(`Congratulations, ${userName}!`);
-      return;
-    }
-
-    const a = getRandom(0, 11);
-    const b = getRandom(0, 11);
+  const userName = startMessage('What is the result of the expression?');
+  const count = 0;
+  const qestionFun = (() => {
+    const a = getRandom(0, 10);
+    const b = getRandom(0, 10);
     const operator = getRandom(0, 3);
-    const expression = getExpression(a, b, operator);
-    const correctAnswer = getCorrectAnswer(a, b, operator);
 
-    const userAnswer = readlineSync.question(`Question: ${expression} `);
-    console.log(`Your answer: ${userAnswer}`);
-
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-      startBrainCalc(correctAnswersCount + 1);
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+    switch (operator) {
+      case 0: return `${a} + ${b}`;
+      case 1: return `${a} * ${b}`;
+      case 2: return `${a} - ${b}`;
+      default: return 'error';
     }
-  };
+  });
+  const correctAnswerFun = ((qestion) => {
+    const a = Number(qestion[0]);
+    const b = Number(qestion[4]);
+    const operator = qestion[2];
 
-  return startBrainCalc(0);
+    switch (operator) {
+      case '+': return String(a + b);
+      case '*': return String(a * b);
+      case '-': return String(a - b);
+      default: return 'error';
+    }
+  });
+
+  return startGame(count, correctAnswerFun, userName, qestionFun);
 };
