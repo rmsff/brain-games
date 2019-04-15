52b4fa7573fae00d9f978d6008136599c816665a
Serikoff <serikoff@li.ru>
2019-04-15 09:24:42 +0300

    fixed bugs

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,8 +1,3 @@
 #!/usr/bin/env node
 
-console.log('Hi! Choose a game from the list and just enter the name of the game!');
-console.log('brain-calc');
-console.log('brain-even');
-console.log('brain-gcd');
-console.log('brain-prime');
-console.log('brain-progression');
+console.log('Hi! Choose a game from the list and just enter the name of the game!\nbrain-calc\nbrain-even\nbrain-gcd\nbrain-prime\nbrain-progression');
