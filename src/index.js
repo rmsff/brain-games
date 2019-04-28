3d521e9172650eb9be41a910d93e5a76008ae2ee
Serikoff <serikoff@li.ru>
2019-04-28 09:20:14 +0300

    minor code improvements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import readlineSync from 'readline-sync';
 
 const numberOfIterations = 3;
+const userName = readlineSync.question('May I have your name?  ');
 
 const startGame = (getGameData, message) => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?  ');
   console.log(`Hello, ${userName}!`);
   console.log(message);
   const iter = (count) => {
     if (count === numberOfIterations) {
       console.log(`Congratulations, ${userName}!`);
+      replay();
       return;
     }
     const gameData = getGameData();
@@ -21,10 +22,28 @@ const startGame = (getGameData, message) => {
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gameData.answer}'.`);
       console.log(`Let's try again, ${userName}!`);
+      replay();
       return;
     }
     iter(count + 1);
   };
+
+  const replay = () => {
+    const answerReplay = readlineSync.question('To play again? (yes/no) ');
+
+    if (answerReplay === 'yes') {
+      const answerReplaySameGame = readlineSync.question('Play the same game? (yes/no) ');
+      if (answerReplaySameGame === 'yes') {
+        iter(0);
+      } else {
+        console.log('Choose a game from the list and just enter the name of the game!\nbrain-calc\nbrain-even\nbrain-gcd\nbrain-prime\nbrain-progression');
+        return;
+      }
+    } else {
+      console.log('Game over!\nBye-Bye!');
+    }
+  };
+
   iter(0);
 };
 
